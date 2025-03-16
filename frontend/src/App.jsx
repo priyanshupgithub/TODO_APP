5215ab1e3432f4d5a028eaa1f5bf02a63e358102
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-03-16 16:12:11 +0530

    update the frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import "./App.css";
-import { BrowserRouter, Route , Routes} from "react-router";
+import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { Navigate, Route, Routes } from "react-router-dom";
 import PageNotFound from "./components/PageNotFound";
-
+import { Toaster } from "react-hot-toast";
 function App() {
+  const token = localStorage.getItem("jwt");
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={token ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 }
 
--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const PageNotFound = () => {
+function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <>
+      <div className="flex h-screen items-center justify-center space-x-2 flex-col">
+        <div className="text-2xl">404</div>
+        <div className="text-xl font-semibold text-green-900">
+          Page Not Found
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
